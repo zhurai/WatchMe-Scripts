01411efc0277fb33fc21b8538e7447f8bf9361b9
zhurai <zhurai@users.noreply.github.com>
2019-03-25 07:17:11 -0700

    debug

--- BAT/TEST/gdrivetemplate.bat
+++ BAT/TEST/gdrivetemplate.bat
@@ -1,5 +1,6 @@
 @echo off
 
+cd %~dp0
 set s=%~dp0
 set b=%~n0
 for /F "tokens=1,2,3,4,5,6,7,8 delims=\" %%a in ("%s%") do (
@@ -15,4 +16,8 @@ for /F "tokens=1,2,3,4,5,6,7,8 delims=\" %%a in ("%s%") do (
 cd %~dp0
 cd ..\..
 
+echo %h%
+echo "%~n0"
+
 C:\Python\pythonw.exe finished.py %h% "%~n0"
+cd %~dp0
