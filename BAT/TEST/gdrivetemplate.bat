ae0bce7a40840ca528a68b990399754fed413ad0
zhurai <zhurai@users.noreply.github.com>
2019-03-24 17:20:29 -0700

    fix the gdrivetemplate to test the rest

--- BAT/TEST/gdrivetemplate.bat
+++ BAT/TEST/gdrivetemplate.bat
@@ -2,7 +2,7 @@
 
 set s=%~dp0
 set b=%~n0
-for /F "tokens=1,2,3,4,5,6,7,8,9,10 delims=\" %%a in ("%s%") do (
+for /F "tokens=1,2,3,4,5,6,7,8 delims=\" %%a in ("%s%") do (
    set a=%%a
    set b=%%b
    set c=%%c
@@ -10,10 +10,9 @@ for /F "tokens=1,2,3,4,5,6,7,8,9,10 delims=\" %%a in ("%s%") do (
    set e=%%e
    set f=%%f
    set g=%%g
-   set h=%%h
 )
 
-echo %h%
+echo %g%
 echo %~n0
 
-C:\Python\pythonw.exe "C:\Users\WatchMe\Google Drive\Programs-WatchMe\Files\finished.py" %h% "%~n0"
+C:\Python\pythonw.exe ..\..\finished.py" %h% "%~n0"
