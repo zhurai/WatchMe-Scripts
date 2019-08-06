ff810cd52732d7f604ff354cca647b7be7660560
zhurai <zhurai@users.noreply.github.com>
2019-08-06 08:15:42 -0700

    E7 update

---
+++ BAT/ES/Secret Shop Refreshed.bat
@@ -0,0 +1,19 @@
+@echo off
+
+cd %~dp0
+set s=%~dp0
+set b=%~n0
+for /F "tokens=1,2,3,4,5,6,7,8 delims=\" %%a in ("%s%") do (
+   set a=%%a
+   set b=%%b
+   set c=%%c
+   set d=%%d
+   set e=%%e
+   set f=%%f
+   set g=%%g
+)
+
+cd %~dp0
+cd ..\..
+C:\Python\pythonw.exe finished.py %g% "%~n0"
+cd %~dp0
